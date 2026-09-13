681cd0a868f9460538d41d4df5aa6d6eec1145c9
Agustin Espina <agustinespina.ae@gmail.com>
2026-09-12 23:26:00 -0300

    TP01 Ej08 Resuelto - TP01 Ej02 Remodelado

--- ejercicios/tp01_funciones/tp01_ej02_fecha_valida.py
+++ ejercicios/tp01_funciones/tp01_ej02_fecha_valida.py
@@ -5,33 +5,38 @@ Devolver True o False según la fecha sea correcta o no. Realizar también un
 programa para verificar el comportamiento de la función.'''
 
 def validar_fecha(fecha: tuple[int, int, int]) -> bool:
-    '''Contrato: Recibe 3 int correspondientes a dia, mes y anio. Verifica que sean una fecha valida.
-        Precondiciones: Primer argumento 'dia' entre 1 y 31, segundo argumento 'mes' entre 1 y 12 y tercer argumento 'anio' entre -10_000 y 10_000.
-        Postcondiciones: True si la fecha es valida, False si no lo es.'''
+    '''
+    Contrato:
+        Recibe 3 int correspondientes a dia, mes y año. Verifica que sean una fecha válida.
+    Precondiciones:
+        Primer argumento 'dia' entre 1 y 31, segundo argumento 'mes' entre 1 y 12 y tercer argumento 'anio' entre -10_000 y 10_000.
+    Postcondiciones:
+        True si la fecha es válida, False si no lo es.
+    '''
     dias = [n for n in range(1, 32)]
     meses = [n for n in range(1, 13)]
-    anios = [n for n in range(-10_000, 10_001)]
+    anios = [n for n in range(0, 2222)]
 
     dia, mes, anio = fecha
 
     if dia not in dias or mes not in meses or anio not in anios:
         return False
 
-    bisiesto = es_bisiesto(anio)
+    dias_por_mes = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    ultimo_dia = dias_por_mes[mes - 1]
 
-    if dia < 29 or (dia < 30 and bisiesto):
-        return True
-    if dia == 30 and mes in [4, 6, 9, 11]:
-        return True
-    if dia == 31 and mes in [1, 3, 5, 7, 8, 10, 12]:
-        return True
+    if mes == 2 and es_bisiesto(anio):
+        ultimo_dia = 29
 
-    return False
+    return dia <= ultimo_dia
 
 def es_bisiesto(anio: int) -> bool:
     ''' 
-        Contrato: Verifica si el anio es bisiesto o no.
-        Precondiciones: Que el numero sea un anio real.
-        Postcondiciones: True si es anio bisiesto, False si no lo es.
+    Contrato:
+        Verifica si el año es bisiesto o no.
+    Precondiciones:
+        Que el numero sea un año real.
+    Postcondiciones:
+        True si es año bisiesto, False si no lo es.
     '''
     return anio % 400 == 0 or (anio % 4 == 0 and anio % 100 != 0)
--- ejercicios/tp01_funciones/tp01_ej08_calendario_mes.py
+++ ejercicios/tp01_funciones/tp01_ej08_calendario_mes.py
@@ -5,7 +5,9 @@ imprimir por pantalla el calendario de un mes completo, correspondiente a un mes
 y año cualquiera basándose en la función suministrada. Considerar que la semana
 comienza en domingo.'''
 
-def diadelasemana(dia,mes,año):
+import tp01_ej02_fecha_valida as fv
+
+def diadelasemana(dia,mes,año): #Le falta DOCSTRING y Typehints, insalubre profe!!
     if mes < 3:
         mes = mes + 10
         año = año - 1
@@ -17,3 +19,40 @@ def diadelasemana(dia,mes,año):
     if diasem < 0:
         diasem = diasem + 7
     return diasem
+
+def main():
+    '''Programa principal'''
+
+    print('='*50)
+    print('Bienvenido a tu calendario.')
+    print('='*50)
+
+    mes = 0
+    while mes < 1 or mes > 12:
+        mes = int(input('Ingrese el mes a visualizar (del 1 al 12): '))
+
+    anio = 0
+    while anio < 1900 or anio > 2100:
+        anio = int(input('Ingrese el año a visualizar (del 1900 al 2100): '))
+    
+    calendar = []
+    for dia in range(1, 32):
+        fecha = dia, mes, anio
+        if fv.validar_fecha(fecha):
+            calendar.append(fecha)
+
+    dias_semana = []
+    for i in range(len(calendar)):
+        dias_semana.append(diadelasemana(*calendar[i]))
+
+    calendario = list(zip(calendar, dias_semana))
+
+    print('=====CALENDARIO=====')
+    for dias in calendario:
+        nombre_dia = {0: 'Domingo', 1: 'Lunes', 2: 'Martes', 3: 'Miercoles', 4: 'Jueves', 5: 'Viernes', 6: 'Sabado'}
+        print(f'El dia {dias[0][0]} cae {nombre_dia[dias[1]]}')
+    print('=====CALENDARIO=====')
+
+
+if __name__ == '__main__':
+    main()
